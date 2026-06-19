7ea77f2fc87d2baca3befb622c29257324cc7ef5
Aadhya Reddy <shakkuaadhi@gmail.com>
2026-06-19 13:36:12 +0000

    simple test model

--- models/01_core/02_service/core_trip.sql
+++ models/01_core/02_service/core_trip.sql
@@ -1,17 +1,3 @@
-
-
-select distinct
-            table_schema as "table_schema",
-            table_name as "table_name",
-            
-            case table_type
-                when 'BASE TABLE' then 'table'
-                when 'EXTERNAL TABLE' then 'external'
-                when 'MATERIALIZED VIEW' then 'materializedview'
-                else lower(table_type)
-            end as "table_type"
-
-        from information_schema.tables
-        where table_schema like 'dbo'
-        and table_name like '%'
-        and table_name not like ''
+select
+    1 as id,
+    'Fabric Git Success' as test_name
